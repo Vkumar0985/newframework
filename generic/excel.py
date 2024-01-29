378452bf87662f5e0f4986f37c031f9913bf251f
vkumar628 <vijaykss09@gmail.com>
2024-01-29 12:00:18 +0530

    first commit

--- generic/excel.py
+++ generic/excel.py
@@ -8,3 +8,13 @@ class Excel:
             return v
         except:
             return ""
+
+    @staticmethod
+    def write_cell_data(path, sheet, row, col):
+        try:
+            wb=openpyxl.load_workbook(path)
+            v = wb[sheet].cell(row, col).value
+            return v
+        except:
+            return ""
+
--- scripts/test_create_order.py
+++ scripts/test_create_order.py
@@ -5,10 +5,10 @@ import pytest
 from generic.base_setup import Base_SetUp
 from pom.OrderManagement_Page import OrderCreationPage
 from generic.excel import Excel
-from scripts.test_valid_login import Test_ValidLogin
+from pom.login_page import LoginPage
 
 
-class Test_Create_Order:
+class Test_Create_Order(Base_SetUp):
 
     # @pytest.mark.run(order=1)
     def test_creating_order(self):
@@ -22,25 +22,47 @@ class Test_Create_Order:
         item_code = Excel.get_cell_data("../test_data/input.xlsx", "CreateOrder", 2, 8)
 
         # 1.login to the application
-        login_to_saas = Test_ValidLogin(self)
-        login_to_saas.test_valid_login()
+        #login_to_saas = Test_ValidLogin(self)
+        #login_to_saas.test_valid_login()
+
+        un = Excel.get_cell_data("../test_data/input.xlsx", "validlogin", 2, 1)
+        pw = Excel.get_cell_data("../test_data/input.xlsx", "validlogin", 2, 2)
+        # 1. Enter Valid UN
+        login_page = LoginPage(self.driver)
+        # self.driver.implicitly_wait(10)
+        login_status = login_page.verify_login_page_displayed(self.wait)
+        assert login_status
+        login_page.set_username(un)
+        print("Entered the username:", un)
+        # 2. Enter Valid PW
+        login_page.set_password(pw)
+        print("Entered the Password:", pw)
+        # 3. Click on login Button
+        login_page.click_signin_button()
+        time.sleep(5)
+        login_page.click_link()
+        time.sleep(15)
+        # 4. Verify that Home page is Displayed
+        # homepage = LoginPage(self.driver)
+        status = login_page.verify_homepage_displayed(self.wait)
+        assert status
 
         # 2. Creating an order
-        ocp = OrderCreationPage(self)
+        ocp = OrderCreationPage(self.driver)
         ocp.click_order_management()
         time.sleep(5)
         op_status = ocp.verify_create_order_page_displayed(self.wait)
         assert op_status
         time.sleep(5)
         ocp.click_create_order()
-        time.sleep(5)
+        time.sleep(10)
         ocp.click_drop_down()
         ocp.click_search_link()
         ocp.set_reg_id(reg_id)
         # ocp.click_reg_id_sel()
         ocp.search_btn()
         ocp.ok_btn_sel()
-        time.sleep(5)
+        time.sleep(10)
         # ocp.bill_to_drop_down()
         # ocp.bill_to_acc_sel()
         ocp.ship_to_dropdown_click()
@@ -50,14 +72,18 @@ class Test_Create_Order:
         ocp.ship_to_search_click()
         # ocp.site_no_click()
         ocp.site_no_ok()
+        time.sleep(5)
         ocp.action_click()
         ocp.edit_info_click()
         time.sleep(5)
         ocp.set_ware_house_code(ware_house_code)
+        time.sleep(5)
         ocp.set_req_point_code(req_point_code)
         ocp.set_req_point_name(req_point_name)
         ocp.set_district_code(district_code)
+        time.sleep(5)
         ocp.set_depot_reference(depot_reference)
+        time.sleep(5)
         ocp.add_info_ok_click()
         time.sleep(5)
         ocp.set_item_code(item_code)
@@ -66,7 +92,7 @@ class Test_Create_Order:
         assert add_btn_display
         ocp.set_qty(qty="5")
         ocp.click_add_line()
-        time.sleep(5)
+        time.sleep(10)
         item_added = ocp.verify_item_is_added(self.wait)
         assert item_added
         ocp.click_submit()
